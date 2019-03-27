5fcb6c1f82da2cda9cf4bd1f7cb7ec30e2149941
Karsten van der Deijl <karsten_vanderdeijl@fis.edu>
2019-03-27 19:19:48 +0100

    main script

--- main-script/main-script.ino
+++ main-script/main-script.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <FastLED.h>
 
+const int LEDpower = 250;
+const int LEDpin = 6;
+const int pinMatrix[4][4] = {{22, 23, 24, 25}, {26, 27, 28, 29}, {30, 31, 32, 33}, {34, 35, 36, 37}};
+const int numLEDs = 16;
+const int LEDmatrix[4][4] = {{0, 7, 8, 15}, {1, 6, 9, 14}, {2, 5, 10, 13}, {3, 4, 11, 12}}; //1 LED per square
+
+CRGB leds[numLEDs];
+
+void setup() {
+  FastLED.addLeds<WS2811, LEDpin, GRB>(leds, numLEDs);
+  for(int i : pinMatrix){
+    pinMode(i, INPUT);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    for(int x = 0; x <= 3; x ++){
+      for (int y = 0; y <= 3; y ++){
+        if(digitalRead(pinMatrix[x][y])) leds[LEDmatrix[x][y]] = CRGB(LEDpower, LEDpower, LEDpower);
+     }
+   }
+   FastLED.show();
 }
--- test scripts/LEDmatrix-test/LEDmatrix-test.ino
+++ test scripts/LEDmatrix-test/LEDmatrix-test.ino
@@ -5,14 +5,40 @@
 int matrix[4][4] = {{0, 7, 8, 15}, {1, 6, 9, 14}, {2, 5, 10, 13}, {3, 4, 11, 12}}; 
 
 CRGB leds[numLEDs];
+String out[];
 
 
 void setup() {
+  Serial.begin(9600);
   FastLED.addLeds<WS2811, LEDpin, GRB>(leds, numLEDs);
-  leds[matrix[0][0]] = CRG
+}
+
+void cut(String in, char sep){
+  int prev = 0;
+  int count = 0;
+  String med[10];
+  
+  for(int p = 0; p < in.length(); p ++){
+    if(in[p] == sep){
+      med[count] = in.substring(prev, p - 1);
+      prev = p + 1;
+      count ++;
+    }
+  }
+
+  int c = 0;
+  String out[count];
+  for(String i : med){
+    if(i != NULL){
+      out[c] = i;
+      c ++;
+    }
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  cut("yo, qo", ',');
+  for(String i : cutted){
+    Serial.println(i);
+  }
 }
